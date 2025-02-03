fbccb52e3b8ba61a139721ea9784e22c0660563f
mr-raushan <kraushan9056@gmail.com>
2025-02-03 17:29:51 +0530

    "pages added"

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from "./components/common/Navbar";
 import About from "./Pages/About";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import ForgotPassword from "./Pages/ForgotPassword";
+import VerifyEmail from "./Pages/VerifyEmail";
+import UpdatePassword from "./Pages/UpdatePassword";
 
 function App() {
   return (
@@ -11,9 +15,54 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />
       </Routes>
     </div>
   );
